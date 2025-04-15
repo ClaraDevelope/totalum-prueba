25b6503276b4582fe48c28a3a4dc49c26d14f344
Clara-Manzano <clara.manzano.corona@gmail.com>
2025-04-15 12:32:17 +0200

    🔥 Elimino archivo mal nombrado y dejo el secreto fuera de Git

--- src/enviroments/enviroment.secret.ts
+++
@@ -1,6 +0,0 @@
-export const secretEnvironment = {
-  totalumApiKey: {
-    'api-key':
-      'sk-eyJrZXkiOiI3MTA1YWUxYzIyN2M3YWQzMzcyZWFhZmMiLCJuYW1lIjoiRGVmYXVsdCBBUEkgS2V5IGF1dG9nZW5lcmF0ZWQgdTR2NiIsIm9yZ2FuaXphdGlvbklkIjoiY2xhcmEtbWFuemFuby1wcnVlYmEtdGVjbmljYSJ9',
-  },
-};
